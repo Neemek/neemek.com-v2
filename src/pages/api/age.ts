a30e3306713c6af3672186a53d1546c7a71cf42f
Sindre F. Ødegaard <sindodeg@icloud.com>
2023-02-20 18:33:37 +0100

    Added `age` api

---
+++ src/pages/api/age.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  age: number
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ age: Math.floor((Date.now()-new Date(2007, 4, 10, 1, 30).getTime())/(1000*60*60*24*365)) })
+}
