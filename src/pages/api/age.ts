d7ac93e29a85ffa79118a3008a1a074f6f65161a
Sindre F. Ødegaard <sindodeg@icloud.com>
2023-02-22 12:04:27 +0100

    Moved age calculations to `calcAge`

--- src/pages/api/age.ts
+++ src/pages/api/age.ts
@@ -4,9 +4,11 @@ type Data = {
   age: number
 }
 
+export const calcAge = () => Math.floor((Date.now()-new Date(2000, 0).getTime())/(1000*60*60*24*365))
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ age: Math.floor((Date.now()-new Date(2000, 0).getTime())/(1000*60*60*24*365)) })
+  res.status(200).json({ age: calcAge() })
 }
