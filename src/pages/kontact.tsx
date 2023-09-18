f8d1ef17c60c3fe530a2f311109ee4664f056489
Neemek <sindodeg@gmail.com>
2023-09-18 21:00:51 +0200

    Update contact page

--- src/pages/kontact.tsx
+++ src/pages/kontact.tsx
@@ -2,14 +2,11 @@ import Head from "next/head"
 import { Inter } from "@next/font/google"
 import styles from '@/styles/Home.module.css'
 import { AnimatedGradientText, OGHead, SideBar, colors } from "."
-import { FaDiscord, FaStackOverflow, FaTwitter } from 'react-icons/fa'
-import { useState } from 'react';
+import { FaDiscord, FaStackOverflow, FaTwitter, FaEnvelope, FaGithub } from 'react-icons/fa'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function ContactPage() {
-  const [email, setEmail] = useState('')
-
   return (
     <>
       <Head>
@@ -25,20 +22,12 @@ export default function ContactPage() {
           <div>
             <h1 className={inter.className}><AnimatedGradientText from={(colors[5] ?? '')[0]} to={(colors[5] ?? '')[1]}>Kontact</AnimatedGradientText> me</h1>
           </div>
-          <div>
-            <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <button onClick={() => {
-              if (email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
-                fetch("https://spaget.neemek.com/email", {
-                  method: 'POST',
-                  body: `{"email": "${email}"}`,
-                  headers: {
-                    "Content-Type": "application/json"
-                  }
-                })
-                setEmail('')
-              }
-            }}>Submit</button>
+          <div className={styles.contacting}>
+            <a className={styles.contactLink} href="mailto:me@neemek.com"><FaEnvelope /> me@neemek.com</a>
+            <a className={styles.contactLink} href="https://twitter.com/neemek_"><FaTwitter /> @Neemek_</a>
+            <a className={styles.contactLink} href="https://discord.gg/HFM8DqhngE"><FaDiscord /> Neemek</a>
+            <a className={styles.contactLink} href="https://stackoverflow.com/users/19857885/neemek"><FaStackOverflow /> Neemek</a>
+            <a className={styles.contactLink} href="https://github.com/Neemek"><FaGithub /> Neemek</a>
           </div>
           <div></div>
         </main>
